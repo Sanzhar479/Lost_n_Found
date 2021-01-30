bf1a43397a4b59ef62431f09aba1102cab1a2e11
Sanzhar Ualiyev <Sanjayzr479@gmail.com>
2021-01-29 23:55:35 -0500

    update

--- LostnFound/Assets/Code/Movement.cs
+++ LostnFound/Assets/Code/Movement.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
     private Rigidbody2D rb;
-    Vector2 movement;
-    private bool jumped=false;
-    public float jumpSpeed;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float CheckRadius;
+    public LayerMask whatIsGround;
+    public int havejumped;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,31 +25,47 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") || jumped == false)
+        if (isGrounded == true)
+        { havejumped = 0; }
+        if (Input.GetKeyDown(KeyCode.W) && havejumped > 0)
         {
-            movement.y = 1.0f;
-            jumped = true;
-            
-       }
-        else { movement.y = 0f; }
+            rb.velocity = Vector2.up * jumpForce;
+            havejumped--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && havejumped == 0 && isGrounded==true) 
+        { rb.velocity = Vector2.up * jumpForce;}
+        
     }
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Obstacle")
         {
-            jumped = false;
+
             Debug.Log("touching");
         }
-        
+
     }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
     private void FixedUpdate()
     {
-        Vector2 pos;
-        pos.x = rb.position.x + movement.x * speed * Time.fixedDeltaTime;
-        pos.y = rb.position.y + movement.y * jumpSpeed * Time.fixedDeltaTime;
-
-        rb.MovePosition(pos);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, CheckRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (facingRight == false && moveInput > 0)
+        {
+            flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            flip();
+        }
     }
 }
